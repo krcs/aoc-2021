c7a6e867ee538a2282c6b734a8923ed325e0ce4d
Krzysztof Cieslak <k.netpost@gmail.com>
2021-12-17 13:57:52 +0100

    17/1

--- 17/day17_1.py
+++ 17/day17_1.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import re
-
-#source = "./input.txt"
-source = "./test.txt"
+source = "./input.txt"
+#source = "./test.txt"
 
 def read_data(file):
+    result = { 'x' : [], 'y' : [] }
     with open(file,"r") as f:
             line = f.read().strip()
             area = re.findall("[xy]=-?\d+[.]{2}-?\d+",line)
-
+            for a in area:
+                result[a[0]] = list(map(lambda n: int(n),a[2:].split("..")))
+    return result
 
 target_area = read_data(source)
-print(target_area)
+y = target_area['y'][0]*-1-1
+result = y*(y+1)/2
+print(int(result))
